c09e8bc9ec277aacab579236ee0a8ef695cf86a4
Nafeez Ahamed <yourwhitehat@yahoo.com>
2011-10-04 13:20:17 +0530

    Penultimate Commit

--- blender.c
+++ blender.c
@@ -1,3 +1,8 @@
+/*  Author : Nafeez Ahamed , github.com/skepticfx */
+/* Enter this IPTABLE ENTRY TO test this PoC */
+/* iptables -A INPUT -p tcp --destination-port 80 -j DROP */
+/* Later , iptables -D INPUT 1 */
+
 #include <pcap.h> 
 #include <stdlib.h> 
 #include <string.h> 
@@ -15,12 +20,12 @@
 int main(int argc, char *argv[]){
  int syn_received = 0 , syn_sent = 0 , established = 0;
  int i=0; 
- bpf_u_int32 netaddr=0, mask=0;    /* To Store network address and netmask   */ 
- struct bpf_program filter;        /* Place to store the BPF filter program  */ 
- char errbuf[PCAP_ERRBUF_SIZE];    /* Error buffer                           */ 
- pcap_t *descr = NULL;             /* Network interface handler              */ 
- struct pcap_pkthdr pkthdr;        /* Packet information (timestamp,size...) */ 
- const unsigned char *packet=NULL; /* Received raw data                      */ 
+ bpf_u_int32 netaddr=0, mask=0;    
+ struct bpf_program filter;       
+ char errbuf[PCAP_ERRBUF_SIZE];  
+ pcap_t *descr = NULL;            
+ struct pcap_pkthdr pkthdr;       
+ const unsigned char *packet=NULL; 
  memset(errbuf,0,PCAP_ERRBUF_SIZE); 
  
  struct ip *iphdr = NULL;
@@ -30,25 +35,22 @@ if (argc != 2){
     printf("USAGE: %s <interface>\n",argv[0]); 
     exit(1); 
 } 
- /* Open network device for packet capture */ 
+
  if ((descr = pcap_open_live(argv[1], MAXBYTES2CAPTURE, 0,  0, errbuf))==NULL){
     fprintf(stderr, "ERROR: %s\n", errbuf);
     exit(1);
  }
-    
- /* Look up info from the capture device. */ 
+
  if( pcap_lookupnet( argv[1] , &netaddr, &mask, errbuf) == -1){
      fprintf(stderr, "ERROR: %s\n", errbuf);
     exit(1);
  }
 
- /* Compiles the filter expression into a BPF filter program */ 
  if ( pcap_compile(descr, &filter, "(tcp[tcpflags] & (tcp-syn) != 0) ||  (tcp[tcpflags] & (tcp-ack & tcp-syn) != 0)", 1, mask) == -1){
     fprintf(stderr, "ERROR: %s\n", pcap_geterr(descr) );
     exit(1);
  }
 
- /* Load the filter program into the packet capture device. */ 
  if (pcap_setfilter(descr,&filter) == -1){
     fprintf(stderr, "ERROR: %s\n", pcap_geterr(descr) );
     exit(1);
@@ -58,7 +60,7 @@ if (argc != 2){
  while(1){ 
  
   if ( (packet = pcap_next(descr,&pkthdr)) == NULL){  /* Get one packet */ 
-    fprintf(stderr, "ERROR: Error getting the packet.\n", errbuf);
+    fprintf(stderr, "ERROR: Error getting the packet. \n", errbuf);
     exit(1);
  }
  
@@ -66,7 +68,7 @@ if (argc != 2){
   tcphdr = (struct tcphdr *)(packet+14+20);
   
   if(!established){
-  printf("\n\n---------------------------------------------------------------------------------------------------------------------------------\n"); 
+  printf("\n---------------------------------------------------------------------------------------------------------------------------------\n"); 
   printf("\nReceived Packet (%d) of Size: %d bytes\n", ++i, pkthdr.len);
    
    
@@ -74,18 +76,12 @@ if (argc != 2){
    
    printf("%s : %d -->", inet_ntoa(iphdr->ip_dst) , ntohs(tcphdr->th_dport));
    
-  printf("\t Flags -- 0x%X  (%s)\n", tcphdr->th_flags, (tcphdr->th_flags == 0x02)?"SYN":"SYN-ACK"); // 0x12 is syn-ack
-  
-  
-
+   printf("\t Flags -- 0x%X  (%s)\n", tcphdr->th_flags, (tcphdr->th_flags == 0x02)?"SYN":"SYN-ACK"); // 0x12 is syn-ack
   
   
   // Now that the sniffing is done for syn or syn-acks , lets process them.
   
   /* If a new SYN is received */
-  
-
-  
   
   if(tcphdr->th_flags == 0x02 && !syn_received && tcphdr->th_ack == htonl(0))
  	
